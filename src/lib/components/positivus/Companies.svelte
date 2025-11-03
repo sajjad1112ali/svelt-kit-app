8d655e8d9275009862d5c49487ae771c16eeead5
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-03 12:12:05 +0500

    feat(layout): add companies

---
+++ src/lib/components/positivus/Companies.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<section class="pt-[70px]">
+	<div class="">
+		<div
+			class="grid
+        grid-cols-1
+        gap-x-[95px]
+        sm:grid-cols-2
+        md:grid-cols-3 lg:grid-cols-6  
+        gap-y-10 
+        place-items-center"
+		>
+			<img src="/positivus/amazon.png" alt="Amazon" class="h-[48px] w-auto object-contain" />
+			<img src="/positivus/dribble.png" alt="Uber" class="h-[48px] w-auto object-contain" />
+			<img src="/positivus/hubspot.png" alt="Hubspot" class="h-[48px] w-auto object-contain" />
+			<img src="/positivus/notion.png" alt="Notion" class="h-[48px] w-auto object-contain" />
+			<img src="/positivus/netflix.png" alt="Netflix" class="h-[48px] w-auto object-contain" />
+			<img src="/positivus/zoom.png" alt="Zoom" class="h-[48px] w-auto object-contain" />
+		</div>
+	</div>
+</section>
