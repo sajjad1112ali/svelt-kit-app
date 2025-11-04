f01953068e621e26b0b20bc9947337880ed14729
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-04 13:31:38 +0500

    feat(layout): add header

---
+++ src/lib/components/positivus/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Button from './Button.svelte';
+	import Heading1 from './Heading1.svelte';
+	import Paragraph from './Paragraph.svelte';
+</script>
+
+<section>
+	<div class="grid items-center justify-center gap-12 md:grid-cols-2">
+		<div class="space-y-3 md:space-y-5 xl:space-y-9">
+			<div class="max-w-xl">
+				<Heading1 text="Navigating the digital landscape for success" />
+			</div>
+			<div class="max-w-[498px]">
+				<Paragraph
+					text="Our digital marketing agency helps businesses grow and succeed online through a range of
+				services including SEO, PPC, social media marketing, and content creation."
+				/>
+			</div>
+
+			<Button text="Book a consultation" />
+		</div>
+
+		<div class="flex justify-center md:justify-end">
+			<img
+				src="/positivus/header.png"
+				alt="Hero"
+				class="w-full max-w-md object-contain md:max-w-full"
+			/>
+		</div>
+	</div>
+</section>
---
+++ src/lib/components/positivus/Heading4.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { text, classes = "" }: { text: string; classes?: string } = $props();
+</script>
+
+<h1
+	class="rounded-md box-decoration-clone grotesk px-2 text-xl md:text-2xl lg:text-2xl font-medium {classes}"
+>
+	{text}
+</h1>
