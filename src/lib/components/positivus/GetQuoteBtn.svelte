db59472c361cfdddd25afe6f985a52ddae767ef5
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-05 18:56:02 +0500

    feat(app): add get quote button

---
+++ src/lib/components/positivus/GetQuoteBtn.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { classes }: { classes?: string } = $props();
+</script>
+
+<li>
+	<button
+		class="grotesk rounded-2xl border border-black bg-transparent px-4
+							py-3 text-base
+							text-black md:px-6 md:py-3 md:text-lg lg:px-9 lg:py-5 lg:text-lg"
+	>
+		Request a quote
+	</button>
+</li>
--- src/lib/components/positivus/Navbar.svelte
+++ src/lib/components/positivus/Navbar.svelte
@@ -1,17 +1,46 @@
 <script lang="ts">
 	import { WIDTH_CLASS } from '$lib/components/positivus/utils/constants';
+	import GetQuoteBtn from './GetQuoteBtn.svelte';
+	import Link from './Link.svelte';
+	const links = [
+		{
+			text: 'About us',
+			href: '#',
+			classes: ''
+		},
+		{
+			text: 'Services',
+			href: '#',
+			classes: ''
+		},
+		{
+			text: 'Use Cases',
+			href: '#',
+			classes: ''
+		},
+		{
+			text: 'Pricing',
+			href: '#',
+			classes: ''
+		},
+		{
+			text: 'Blog',
+			href: '#',
+			classes: 'mb-2 md:mb-0'
+		}
+	];
 </script>
 
-<header class="mt-[60px] mb-[70px] border-1 bg-white">
+<header class="mt-[60px] mb-[70px] bg-white">
 	<div
 		class="
-{WIDTH_CLASS} mx-auto flex flex-wrap items-center"
+{WIDTH_CLASS} flex flex-wrap items-center"
 	>
 		<div class="flex flex-1 items-center justify-between">
 			<img src="/positivus/logo.png" alt="Positivus Company Logo" class="h-8 w-auto" />
 		</div>
 
-		<label for="menu-toggle" class="pointer-cursor block md:hidden">
+		<label for="menu-toggle" class="pointer-cursor block lg:hidden">
 			<svg
 				class="fill-current text-gray-900"
 				xmlns="http://www.w3.org/2000/svg"
@@ -25,26 +54,23 @@
 		</label>
 		<input class="hidden" type="checkbox" id="menu-toggle" />
 
-		<div class="hidden w-full md:flex md:w-auto md:items-center" id="menu">
+		<div class="hidden w-full lg:flex lg:w-auto lg:items-center" id="menu">
 			<nav>
 				<ul class="items-center justify-between pt-4 text-base text-gray-700 md:flex md:pt-0">
-					<li><a class="grotesk block px-0 py-3 md:p-4" href="#">About us</a></li>
-					<li><a class="grotesk block px-0 py-3 md:p-4" href="#">Services</a></li>
-					<li><a class="grotesk block px-0 py-3 md:p-4" href="#">Use Cases</a></li>
-					<li><a class="grotesk block px-0 py-3 md:mb-0 md:p-4" href="#">Pricing</a></li>
-					<li><a class="grotesk mb-2 block px-0 py-3 md:mb-0 md:p-4" href="#">Blog</a></li>
+					{#each links as link}
+						<li><Link {...link} /></li>
+					{/each}
+					<GetQuoteBtn />
 				</ul>
 			</nav>
 		</div>
-		<!-- Mobile Menu -->
-		<div class="flex w-full md:hidden md:w-auto md:items-center" id="mobileMenu">
+		<div class="flex w-full lg:hidden lg:w-auto lg:items-center" id="mobileMenu">
 			<nav>
-				<ul class="items-center justify-between pt-4 text-base text-gray-700 md:flex md:pt-0">
-					<li><a class="grotesk block px-0 py-3 md:p-4" href="#">About us...</a></li>
-					<li><a class="grotesk block px-0 py-3 md:p-4" href="#">Services</a></li>
-					<li><a class="grotesk block px-0 py-3 md:p-4" href="#">Use Cases</a></li>
-					<li><a class="mb-2 block px-0 py-3 md:mb-0 md:p-4" href="#">Pricing</a></li>
-					<li><a class="mb-2 block px-0 py-3 md:mb-0 md:p-4" href="#">Blog</a></li>
+				<ul class="items-center justify-between pt-4 text-base text-gray-700 lg:flex md:pt-0">
+					{#each links as link}
+						<li><Link {...link} /></li>
+					{/each}
+					<GetQuoteBtn />
 				</ul>
 			</nav>
 		</div>
