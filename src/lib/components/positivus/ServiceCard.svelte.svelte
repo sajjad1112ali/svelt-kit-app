30657959925d1dae0a8b9cab25171e984dc5bdbd
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-07 20:06:04 +0500

    feat(positivus): add service card

---
+++ src/lib/components/positivus/ServiceCard.svelte.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Heading3 from './Heading3.svelte';
+
+	let { title, image, bg, heading, arrowClass }: { title: string; image: string; bg: string; heading: string; arrowClass: string } =
+		$props();
+</script>
+
+<div class={`overflow-hidden rounded-4xl p-12  border border-black shadow-[0_3px_0_0_rgba(0,0,0,1)] h-[360px] md:h-[310px] ${bg}`}>
+	<div class="grid items-center gap-8 md:grid-cols-2">
+		<div class="flex flex-col items-center md:items-start md:justify-between space-y-6 md:h-[210px]">
+			<Heading3 text={title} classes={heading} />
+			<div
+				class="flex items-center gap-2"
+			>
+				<div class="{arrowClass} p-2 rounded-full rotate-10">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="2.5"
+						stroke="currentColor"
+						class="size-4"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+						/>
+					</svg>
+				</div>
+				<span class="text-xl grotesk font-medium {bg === "bg-black" ? 'text-white' : ''}">Learn More</span>
+			</div>
+		</div>
+
+		<div class="flex justify-center">
+			<img src={image} alt={title} class="max-h-40 object-contain" />
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/positivus/TextBox.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { placeholder, classes }: { placeholder: string; classes?: string } = $props();
+</script>
+
+<input 
+  type="text" 
+  placeholder={placeholder}
+  class="{classes} w-full text-white px-7 py-5 border border-gray-300 rounded-2xl text-lg placeholder-gray-400 focus:outline-none focus:border-gray-400"
+/>
+
