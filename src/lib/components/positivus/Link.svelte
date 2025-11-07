ce10c813845036594241d785e04f52246ac75dbf
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-07 20:02:42 +0500

    feat(positivus): add link component

---
+++ src/lib/components/positivus/Link.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let { text, classes, href }: { text: string; classes?: string; href: string } = $props();
+</script>
+
+<a class="{classes} grotesk block px-0 py-3 text-base md:p-4 md:text-lg lg:text-lg" {href}>{text}</a>
