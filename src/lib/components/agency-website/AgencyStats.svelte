9fb5b1b80c7500c0b98ace50f50ce2d07ab818e1
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-08 19:59:08 +0500

    feat(agency): add agency stats

---
+++ src/lib/components/agency-website/AgencyStats.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { AGENCY_WEBSITE_WIDTH_CLASS } from '$lib/components/positivus/utils/constants';
+	import TextBig from './TextBig.svelte';
+	import Heading3 from './Heading3.svelte';
+	const stats = [
+		{
+			count: '42',
+			lebel: 'Years of experience'
+		},
+		{
+			count: '73+',
+			lebel: 'Agency members'
+		},
+		{
+			count: '5,000',
+			lebel: 'Projects complete'
+		}
+	];
+</script>
+
+<div class="pb-40">
+	<section class={AGENCY_WEBSITE_WIDTH_CLASS}>
+		<div
+			class="grid grid-cols-1 divide-y divide-[#F3D1BF] rounded-md border border-[#F3D1BF] md:grid-cols-3 md:divide-x md:divide-y-0"
+		>
+			{#each stats as stat}
+				<div class="flex flex-col gap-3 py-11 pl-12">
+					<Heading3 text={stat.count} />
+				<TextBig text={stat.lebel}/>
+			</div>
+			{/each}
+		</div>
+	</section>
+</div>
