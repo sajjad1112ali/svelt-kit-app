54749b449d53495b744fd2dafc478de4d556fa0a
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-10 21:08:55 +0500

    feat(agency-website): add companies

---
+++ src/lib/components/agency-website/Companies.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { AGENCY_WEBSITE_WIDTH_CLASS } from '$lib/components/positivus/utils/constants';
+	import Button from './Button.svelte';
+	import Heading5 from './Heading5.svelte';
+	import Heading1 from './Heading1.svelte';
+	import Text from './Text.svelte';
+</script>
+
+<div class="py-18">
+	<section class="{AGENCY_WEBSITE_WIDTH_CLASS}">
+		<div class="flex flex-col md:flex-row justify-between items-center gap-8">
+			<img
+				src="/agency-website/digital-inside.png"
+				class="h-[42px] w-[147px] object-cover"
+				alt="Digital inside"
+			/>
+			<img src="/agency-website/vortex.png" class="h-[32px] w-[108px] object-cover" alt="Vortex" />
+			<img
+				src="/agency-website/travel-explore.png"
+				class="h-[47px] w-[119px] object-cover"
+				alt="Travel Explore"
+			/>
+			<img src="/agency-website/fuzion.png" class="h-[21px] w-[86px] object-cover" alt="Fuzion" />
+			<img
+				src="/agency-website/media-fury.png"
+				class="h-[24px] w-[126px] object-cover"
+				alt="Media Fury"
+			/>
+		</div>
+	</section>
+</div>
+
+<div class="{AGENCY_WEBSITE_WIDTH_CLASS} h-px bg-[#F3D1BF]"></div>
