3241f31d2bf61215dcfca35c4360a5316be20e8f
Sajjad Ali <sajjadramzan1211@gmail.com>
2025-11-14 19:36:38 +0500

    feat(agency-website): add pricing plan

---
+++ src/lib/components/agency-website/LatestBlog.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { AGENCY_WEBSITE_WIDTH_CLASS } from '$lib/components/positivus/utils/constants';
+	import Button from './Button.svelte';
+	import Heading5 from './Heading5.svelte';
+	import Text from './Text.svelte';
+	import Heading2 from './Heading2.svelte';
+	import Heading3 from './Heading3.svelte';
+	import IconContainer from './IconContainer.svelte';
+	import Heading4 from './Heading4.svelte';
+	import TextBig from './TextBig.svelte';
+	const blogs = [
+		{
+			image: '/agency-website/blog1.png',
+			title: 'Agency is a business you hire to outsource',
+			date: '5 Nov, 2021',
+			category: 'Stories'
+		},
+		{
+			image: '/agency-website/blog2.png',
+			title: 'Outsource your digital marketing efforts',
+			date: '29 Oct, 2021',
+			category: 'Design'
+		},
+		{
+			image: '/agency-website/blog3.png',
+			title: 'Your business with a variety of digital',
+			date: '21 Oct, 2021',
+			category: 'Marketing'
+		}
+	];
+</script>
+
+<div class="pb-20">
+	<section class={AGENCY_WEBSITE_WIDTH_CLASS}>
+		<div class="flex gap-4">
+			<div class="grid flex-1 grid-cols-1 gap-4">
+				<Heading5 text="Our Blog" classes="text-[#EF6D58]" />
+				<Heading2 text="Latest Blog Articles" classes="max-w-[336px] text-[#391400]" />
+			</div>
+			<div class="flex items-end justify-end">
+				<Button text="Discover All" classes="flex-1 bg-white text-[#391400]" />
+			</div>
+		</div>
+		<div class="mt-16 grid grid-cols-1 gap-6 rounded-md md:grid-cols-3">
+			{#each blogs as item}
+				<div class="">
+					<img src={item.image} class="w-full object-cover" alt={item.title} />
+					<TextBig text={item.category} classes="pt-6 pb-4" />
+
+					<Heading4 text={item.title} classes="text-[#391400]" />
+
+					<Text text={item.date} classes="text-[#391400]/64 pt-4" />
+				</div>
+			{/each}
+		</div>
+	</section>
+	<div class="{AGENCY_WEBSITE_WIDTH_CLASS} mt-26 h-px bg-[#F3D1BF]"></div>
+</div>
---
+++ src/lib/components/agency-website/PricingPlans.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import { AGENCY_WEBSITE_WIDTH_CLASS } from '$lib/components/positivus/utils/constants';
+	import Button from './Button.svelte';
+	import Heading5 from './Heading5.svelte';
+	import Text from './Text.svelte';
+	import Heading2 from './Heading2.svelte';
+	import Heading3 from './Heading3.svelte';
+	const plans = [
+		{
+			heading: 'Consultation',
+			price: 'Free',
+			isPopular: false,
+			description: 'Your digital marketing efforts, instead of handling in-house.',
+			perks: [
+				{
+					name: 'Brand Design',
+					available: true
+				},
+				{
+					name: 'Market Analysis',
+					available: false
+				},
+				{
+					name: 'Production',
+					available: false
+				}
+			]
+		},
+		{
+			heading: 'Design',
+			price: '$1500',
+			description: 'Provide your business with a variety of digital solutions to promote.',
+			isPopular: true,
+			perks: [
+				{
+					name: 'Brand Design',
+					available: true
+				},
+				{
+					name: 'Market Analysis',
+					available: true
+				},
+				{
+					name: 'Production',
+					available: false
+				}
+			]
+		},
+		{
+			heading: 'Design+Code',
+			price: '$2900',
+			description: 'Help you hit your marketing goals and grow your business.',
+			isPopular: false,
+			perks: [
+				{
+					name: 'Brand Design',
+					available: true
+				},
+				{
+					name: 'Market Analysis',
+					available: true
+				},
+				{
+					name: 'Production',
+					available: true
+				}
+			]
+		}
+	];
+</script>
+
+<div class="pb-40">
+	<section class={AGENCY_WEBSITE_WIDTH_CLASS}>
+		<div class="grid grid-cols-1 gap-4">
+			<Heading5 text="Pricing" classes="text-[#EF6D58]" />
+			<Heading3 text="Check Our Pricing Plans" classes="max-w-[358px]" />
+		</div>
+		<div class="flex flex-col items-end justify-center md:flex-row">
+			{#each plans as item}
+				<div class={`w-full rounded-sm border border-gray-300 px-12  md:w-1/3 ${item.isPopular ? "py-8 bg-white shadow-lg " : "py-11" }`}>
+					{#if item.isPopular}
+						<Button text="Popular" classes="bg-[#EF6D58] text-white mb-5.5 !rounded-full" />
+					{/if}
+					<Heading5 text={item.heading} classes="text-[#EF6D58]" />
+					<Heading3 text={item.price} classes="mt-4.5" />
+					<Text text={item.description} classes="text-[#391400] mt-4" />
+
+					<div class="mt-7 flex flex-col gap-8">
+						{#each item.perks as perk}
+							<div class="flex items-center gap-3">
+								<div
+									class="flex h-4 w-4 items-center justify-center rounded-full bg-[#EF6D58] text-white"
+								>
+									<span>+</span>
+								</div>
+								<Text text={perk.name} classes="text-[#391400]" />
+							</div>
+						{/each}
+					</div>
+
+					<Button text="Contact Us" classes={!item.isPopular ? "text-black bg-white mt-9" : "bg-[#EF6D58] text-white mt-9"} />
+				</div>
+			{/each}
+
+			<!-- Plan 2 (Highlighted & Lifted) -->
+			<!-- <div class="-mt-6 w-full border border-gray-300 p-8 text-center shadow-lg md:w-1/3">
+			</div> -->
+		</div>
+	</section>
+</div>
